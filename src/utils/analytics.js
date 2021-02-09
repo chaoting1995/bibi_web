5552e87aa76b5463b64cde87690d88b57f126040
chaoting <02115214@gm.scu.edu.tw>
2021-02-10 00:37:06 +0800

    2021.02.10 00:37

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,26 +1,26 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga';
 // ReactGA.initialize('UA-000000-01');
 // ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const initGA = () => {
-  console.log('GA init')
-  ReactGA.initialize('UA-180233172-1')
-}
+  console.log('GA init');
+  ReactGA.initialize('UA-180233172-1');
+};
 
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`)
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
-}
+  console.log(`Logging pageview for ${window.location.pathname}`);
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 
 export const logEvent = (category = '', action = '') => {
   if (category && action) {
-    ReactGA.event({ category, action })
+    ReactGA.event({ category, action });
   }
-}
+};
 
 export const logException = (description = '', fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal })
+    ReactGA.exception({ description, fatal });
   }
-}
+};
